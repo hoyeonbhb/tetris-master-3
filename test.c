73ceb18cf93b668ba0a0eadcf5de3e5744c82f39
hoyeonbhb <baehoyan@gmail.com>
2023-02-09 20:36:02 +0900

    init

--- test.c
+++ test.c
@@ -12,21 +12,48 @@
 void draw_mainboard_test() {
         for (int i = 0;i < MAIN_Y; i++){
             for (int j = 0;j < MAIN_X; j++){
+                GotoXY(MAIN_Y_ADJ + j, MAIN_X_ADJ + i);
                 if(main_org[i][j] == EMPTY){
-                    GotoXY(MAIN_Y_ADJ + j, MAIN_X_ADJ + i);
                     printw(" ");
                 }
                 if(main_org[i][j] == CEILLING){
-                    GotoXY(MAIN_Y_ADJ + j, MAIN_X_ADJ + i);
+
                     printw("-");
                 }
                 if(main_org[i][j] == WALL){
-                    GotoXY(MAIN_Y_ADJ + j, MAIN_X_ADJ + i);
                     printw("☒");
                 }
             }
         }
-    refresh();
+}
+
+void draw_map_test() {
+    GotoXY(STATUS_X_ADJ, STATUS_Y_LEVEL);
+    printw("LEVEL :     ");
+    printw("%d", level);
+    GotoXY(STATUS_X_ADJ, STATUS_Y_GOAL);
+    printw("GOAL :      ");
+    printw("%d", level_goal);
+    GotoXY(STATUS_X_ADJ, STATUS_Y_LEVEL + 1);
+    printw("+- N E X T -+");
+    for (int i = 0; i < 8; i++){
+        GotoXY(STATUS_X_ADJ, STATUS_Y_LEVEL + 2 + i);
+        printw("|           |");
+    }
+    GotoXY(STATUS_X_ADJ, STATUS_Y_LEVEL + 1 + 9);
+    printw("+- - - - - -+");
+    GotoXY(STATUS_X_ADJ, STATUS_Y_SCORE + 6);
+    printw("your score :");
+    GotoXY(STATUS_X_ADJ, STATUS_Y_SCORE + 7);
+    printw("           %d", score);
+    GotoXY(STATUS_X_ADJ, STATUS_Y_SCORE + 9);
+    printw("last score :");
+    GotoXY(STATUS_X_ADJ, STATUS_Y_SCORE + 10);
+    printw("           %d", last_score);
+    GotoXY(STATUS_X_ADJ, STATUS_Y_SCORE + 12);
+    printw("best score :");
+    GotoXY(STATUS_X_ADJ, STATUS_Y_SCORE + 13);
+    printw("           %d", best_score);
 }
 
 int main(){
@@ -35,6 +62,8 @@ int main(){
     load_master_data();
     GotoXY(2,4);
     draw_mainboard_test();
+    draw_map_test();
+    refresh();
     getch();
     endwin();
     return 0;
